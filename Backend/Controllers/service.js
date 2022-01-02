38d7e5b5d2a191f88167e0b013f559ca41865a21
Dhruv Kacha <dhruvkacha15@gmail.com>
2022-01-02 17:54:13 -0500

    Add and remove doctor api added successfully in services

--- Backend/Controllers/service.js
+++ Backend/Controllers/service.js
@@ -19,7 +19,7 @@ module.exports = {
   },
 
   async getServices(req,res){
-    const services = await Service.find();
+    const services = await Service.find().populate("doctors.doctor_id", "name");
         return res.json(services);
   },
 
@@ -28,6 +28,51 @@ module.exports = {
 
     await Service.findById(service_id).populate("doctors.doctor_id", "name").then((service) => {
       return res.json(service);
+    })
+  },
+
+  async addDoctor(req, res) {
+    const { service_id, doctor_id } = req.body;
+
+    await Service.findByIdAndUpdate(
+      service_id,
+      {
+        $push: {
+          doctors: {
+            doctor_id: doctor_id
+          },
+        },
+      },
+      {
+        new: true,
+      }
+    ).exec((err, result) => {
+      if (err) {
+        return res.json({ error: "Failed to add doctor" });
+      } else {
+        return res.json({result, message:"Doctor added successfully"});
+      }
+    })
+  },
+
+  async removeDoctor(req, res) {
+    const { service_id, doctor_id } = req.body;
+
+    await Service.findByIdAndUpdate(
+      service_id,
+      {
+        $pull: {
+          doctors: {
+            doctor_id: doctor_id
+          },
+        },
+      }
+    ).exec((err, result) => {
+      if (err) {
+        return res.json({ error: "Failed to remove doctor" });
+      } else {
+        return res.json({result, message:"Doctor removed successfully"});
+      }
     })
   }
 };
